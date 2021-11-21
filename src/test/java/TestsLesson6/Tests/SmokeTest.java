e6ceade7f405cfa8ad8d42f3bee66ab78d72fc46
elenaklepec <ckarlettt@rambler.ru>
2021-11-21 20:12:03 +0200

    QA Lesson 6 Added Comments

--- src/test/java/TestsLesson6/Tests/SmokeTest.java
+++ src/test/java/TestsLesson6/Tests/SmokeTest.java
@@ -9,6 +9,9 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class SmokeTest extends BaseTest {
+
+    //Проверка открытия страницы продуктов
+    //после введения верных данных логина и пароля
     @Test
     public void positiveLoginTest() {
 
@@ -22,6 +25,8 @@ public class SmokeTest extends BaseTest {
         Assert.assertEquals(productsPage.getTitleText(),"PRODUCTS","Страница Products не открылась");
     }
 
+    //Проверка появления элемента с ошибкой и текста в нем
+    //после введения некорректрных данных в логин и пароль
     @Test
     public void negativeLoginTest(){
         LoginPage loginPage = new LoginPage(driver,true);
@@ -34,6 +39,8 @@ public class SmokeTest extends BaseTest {
     }
 
     //Homework CartPage, CheckOutPage, ProductsPage and all tests below
+    //Проверка появления на значке с корзиной символа "1"
+    //при добавлении одного товара
     @Test
     public void positiveCartIconTest(){
         LoginPage loginPage = new LoginPage(driver,true);
@@ -47,6 +54,8 @@ public class SmokeTest extends BaseTest {
         Assert.assertEquals(productsPage.getCartQuantityLabel().getText(), "1");
     }
 
+    //Проверка появления выпадающего меню
+    //при нажатии на соответствующую кнопку
     @Test
     public void positiveMenuListTest(){
         LoginPage loginPage = new LoginPage(driver,true);
@@ -58,6 +67,8 @@ public class SmokeTest extends BaseTest {
         productsPage.clickMenuButton();
         Assert.assertTrue(productsPage.getMenuList().isDisplayed());
     }
+
+    //Проверка на появление в корзине товара после добавления его в корзину
     @Test
     public void positiveCartTest(){
         LoginPage loginPage = new LoginPage(driver,true);
@@ -73,6 +84,7 @@ public class SmokeTest extends BaseTest {
 
         Assert.assertEquals(cartPage.getPurchaseInfo().getText(), productsPage.getFirstProduct().getText());
     }
+    //Проверка кнопки удаления товара из корзины
     @Test
     public void negativeCartRemoveTest() {
         LoginPage loginPage = new LoginPage(driver, true);
@@ -89,6 +101,9 @@ public class SmokeTest extends BaseTest {
 
         Assert.assertThrows(org.openqa.selenium.NoSuchElementException.class, () ->cartPage.getPurchaseInfo().isDisplayed());
     }
+
+    //Проверка появления элемента с ошибкой на странице check out
+    //при отсутствии заполненного поля индекса
     @Test
     public void positiveCheckInInfoTest1() {
         LoginPage loginPage = new LoginPage(driver, true);
@@ -111,6 +126,8 @@ public class SmokeTest extends BaseTest {
 
         Assert.assertEquals(checkOutPage.getErrorLabel().getText(), "Error: Postal Code is required");
         }
+    //Проверка появления элемента с ошибкой на странице check out
+    //при отсутствии заполненного поля фамилии
     @Test
     public void positiveCheckInInfoTest2() {
         LoginPage loginPage = new LoginPage(driver, true);
@@ -133,6 +150,8 @@ public class SmokeTest extends BaseTest {
 
         Assert.assertEquals(checkOutPage.getErrorLabel().getText(), "Error: Last Name is required");
     }
+    //Проверка появления элемента с ошибкой на странице check out
+    //при отсутствии заполненного поля имени
     @Test
     public void positiveCheckInInfoTest3() {
         LoginPage loginPage = new LoginPage(driver, true);
@@ -155,6 +174,8 @@ public class SmokeTest extends BaseTest {
 
         Assert.assertEquals(checkOutPage.getErrorLabel().getText(), "Error: First Name is required");
     }
+    //Проверка возвращения на страницу корзины
+    //при нажатии кнопки cancel на странице check out
     @Test
     public void positiveCheckOutCancel() {
         LoginPage loginPage = new LoginPage(driver, true);
